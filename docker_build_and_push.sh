18af8f0ce4000c1fc39df91719300bc7b44561da
Jeroen Knoops <jeroen.knoops@philips.com>
2019-08-15 07:55:07 +0200

    Update docker_build_and_push.sh

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -3,7 +3,7 @@
 set -e
 
 help() { 
-	echo "This script requires two arguments, directory with the Docker file and the DOcker tag.\n"
+  echo "This script requires two arguments, directory with the Docker file and the Docker tag.\n"
   echo -e "Usages: "`basename "$0"` "<docker-file-directory> <tag>"
 } 
 
