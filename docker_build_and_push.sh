76cb5bff2ad1f2d1d634c5fb3fd168b7ae4a600a
Jeroen Knoops <jeroen.knoops@gmail.com>
2020-03-15 08:36:04 +0100

    Fix shell-linter errors in docker_build_and_push.sh

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -3,8 +3,8 @@
 set -e
 
 help() { 
-  echo "This script requires two arguments, directory with the Docker file and the Docker tag.\n"
-  echo -e "Usages: "`basename "$0"` "<docker-file-directory> <tag>"
+  echo "This script requires two arguments, directory with the Docker file and the Docker tag."
+  echo "Usages: $(basename "$0") <docker-file-directory> <tag>"
 } 
 
 if [ "$#" -lt 2 ]; then
@@ -12,7 +12,7 @@ if [ "$#" -lt 2 ]; then
   exit 1
 fi
 
-./docker_build.sh $@
+./docker_build.sh "$@"
 
 echo "PUSH_BRANCH: $PUSH_BRANCH"
 echo "GITHUB_REF: $GITHUB_REF"
@@ -21,7 +21,7 @@ if [[ "$GITHUB_REF" = "refs/heads/$PUSH_BRANCH" ]]
 then
     echo "Matches"
     echo "run docker_push"
-    ./docker_push.sh $@
+    ./docker_push.sh "$@"
 else
     echo "Do not match: $GITHUB_REF != refs/heads/$PUSH_BRANCH"
     exit 0
