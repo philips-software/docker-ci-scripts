cdb48c45174df88a284bcfc4a381a7e1f755cd32
Jie <65960276+jiewuphilips@users.noreply.github.com>
2020-12-10 11:30:23 -0500

    fix bug when a pull_request is the action trigger
    when the action trigger is pull_request, the variable $GITHUB_REF will be something like refs/pull/187/merge, which leads to an unmatched branch error. In this fix, $GITHUB_HEAD_REF is used instead

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -17,15 +17,25 @@ fi
 echo "Check if we need to push the docker images to docker hub:"
 # shellcheck disable=SC2153
 echo "Push branches: $PUSH_BRANCHES"
+echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"
 echo "GITHUB_REF: $GITHUB_REF"
+echo "GITHUB_HEAD_REF: $GITHUB_HEAD_REF"
 
 read -ra push_branches <<< "$PUSH_BRANCHES"
 
 for branch in "${push_branches[@]}"; do
+  if [[ "$GITHUB_EVENT_NAME" = "pull_request" ]]; then
+    if [[ "$GITHUB_HEAD_REF" = "$branch" ]]; then
+      echo "Matches: start pushing"
+      "${FOREST_DIR}"/docker_push.sh "$@"
+      exit 0
+    fi
+  else
   if [[ "$GITHUB_REF" = "refs/heads/${branch}" ]]; then
-    echo "Matches: start pushing"
-    "${FOREST_DIR}"/docker_push.sh "$@"
-    exit 0
+      echo "Matches: start pushing"
+      "${FOREST_DIR}"/docker_push.sh "$@"
+      exit 0
+    fi
   fi
 done
 
