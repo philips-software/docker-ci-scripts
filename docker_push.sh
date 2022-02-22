46fc387f25864142b53ec89b30817095b9a2e8db
Jeroen Knoops <jeroen.knoops@philips.com>
2022-02-22 09:03:08 +0100

    Add some official github action error and notice messages
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- docker_push.sh
+++ docker_push.sh
@@ -7,7 +7,7 @@ docker_organization=$DOCKER_ORGANIZATION
 
 if [ -z "$DOCKER_REGISTRY" ]; then
   if [ -z "$docker_organization" ]; then
-    echo "  No DOCKER_ORGANIZATION set. This is mandatory when using docker.io"
+    echo "::error::No DOCKER_ORGANIZATION set. This is mandatory when using docker.io"
     exit 1
   fi
   DOCKER_REGISTRY="docker.io"
@@ -20,7 +20,7 @@ fi
 echo "docker_registry_prefix: $docker_registry_prefix"
 
 if [ "$#" -lt 4 ]; then
-  echo "You need to provide a directory with a Dockerfile in it, Docker image name, base-dir and a tag."
+  echo "::error::You need to provide a directory with a Dockerfile in it, Docker image name, base-dir and a tag."
   exit 1
 fi
 
@@ -36,12 +36,12 @@ read -ra tags <<<"$alltags"
 basetag=${tags[0]}
 
 if [ -z "$DOCKER_PASSWORD" ]; then
-  echo "  No DOCKER_PASSWORD set. Please provide"
+  echo "::error::No DOCKER_PASSWORD set. Please provide"
   exit 1
 fi
 
 if [ -z "$DOCKER_USERNAME" ]; then
-  echo "  No DOCKER_USERNAME set. Please provide"
+  echo "::error::No DOCKER_USERNAME set. Please provide"
   exit 1
 fi
 
@@ -49,11 +49,11 @@ echo "Login to docker"
 echo "--------------------------------------------------------------------------------------------"
 echo "$DOCKER_PASSWORD" | docker login "$DOCKER_REGISTRY" -u "$DOCKER_USERNAME" --password-stdin
 
-echo "Pushing $docker_registry_prefix/$imagename:$basetag"
+echo "::notice::Pushing $docker_registry_prefix/$imagename:$basetag"
 docker push "$docker_registry_prefix"/"$imagename":"$basetag"
 
 for tag in "${tags[@]:1}"; do
-  echo "Pushing $docker_registry_prefix/$imagename:$tag"
+  echo "::notice::Pushing $docker_registry_prefix/$imagename:$tag"
   docker push "$docker_registry_prefix"/"$imagename":"$tag"
 done
 echo "--------------------------------------------------------------------------------------------"
